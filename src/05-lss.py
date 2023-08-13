41931499c449480f9bbd4c28cac6f50afa4d187e
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-13 18:20:28 -0500

    update

--- src/05-lss.py
+++ src/05-lss.py
@@ -3,20 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_oil_average_curve(data, x, y):
+def plot_oil_average_curve(data1, data2 , x, y):
     plt.figure(figsize=(14,8))
     plt.yscale('log')
     plt.title("Oil - average curve")
     plt.xlabel("TotalProdMonths")
     plt.ylabel("OIL_BBL")
-    plt.legend(loc='center', bbox_to_anchor=(1.12,0.5))
     plt.grid(visible=True, which='major', axis='both')
     
     sns.lineplot(data=data1, x=x, y=y, hue="WellName", palette='Greys', 
     estimator=None, n_boot=1000, lw=0.8, orient='x', sort=True, legend=None, ci='deprecated', ax=None)
     sns.lineplot(data=data2, x=x, y=y, hue=None, palette='Greys', color='C0', 
     estimator=None, n_boot=1000, lw=0.8, orient='x', sort=True, legend=None, ci='deprecated', ax=None)
-    
+
+    plt.legend(loc='center', bbox_to_anchor=(1.12,0.5))
     plt.show()
 
 if __name__ == '__main__':
@@ -26,17 +26,16 @@ if __name__ == '__main__':
     print('=' * 160)
     
     # Transform data
-    max = df.groupby('WellName')['OIL_BBL'].max().reset_index()
-    num_int64 = np.int64(max['OIL_BBL'].mean())  # Calculate the average of the maximum values
-    df.loc[df["TotalProdMonths"] <= 1, 'OIL_BBL'] = num_int64  # Assign the average to values ​​less than or equal to 1
-    df = df[df['OIL_BBL'] <= num_int64]  # Filter values ​​greater than average
+    max_value = df.groupby('WellName')['OIL_BBL'].max().reset_index()
+    avg_max_value = np.int64(max_value['OIL_BBL'].mean())  # Calculate the average of the maximum values
+    df.loc[df["TotalProdMonths"] <= 1, 'OIL_BBL'] = avg_max_value   # Assign the average to values ​​less than or equal to 1
+    df = df[df['OIL_BBL'] <= avg_max_value ]  # Filter values ​​greater than average
     df_mean = df.groupby('TotalProdMonths').mean().reset_index()
 
-    # Example of use of the function
+    # Funtion data
     data1 = df
     data2 = df_mean
     x = 'TotalProdMonths'
     y = "OIL_BBL"
-    plot_oil_average_curve(data1, x, y)
-
+    plot_oil_average_curve(data1, data2, x, y)
 
