1feefbc58a2345c5a25a937a1d4c44e5f155c030
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-23 17:54:32 -0500

    update

--- src/main.py
+++ src/main.py
@@ -4,22 +4,28 @@ import utils
 
 # Read CSV file
 df = pd.read_csv('/home/dasxgo/dev/rocket/out/02-plan.csv')
- 
+print(df.head(5))
+print('-' * 160)
+
 # Display information about the DataFrame
 print(df.info())
 
 # Check data for errors
 error_column = 'OIL_BBL'
 total_errors = df[(df[error_column] == 0) | (df[error_column] < 0)].shape[0]
-    
-# clean the data
+print(f'Total errors in {error_column}: {total_errors}')
+print('-' * 160)
 
+# clean the data
 production_columns = ["OIL_BBL", "GAS_MCF", "WATER_BBL"]
 utils.clean_data(df, production_columns)
 
 # Check after cleaning
 data_okay_column = 'OIL_BBL'
 total_errors_cleaned = df[(df[data_okay_column] == 0) | (df[data_okay_column] < 0)].shape[0]
+print(f'Total errors in {data_okay_column} after cleaning: {total_errors_cleaned}')
+
+print('-' * 160)
 
 # Show first clean records
 print(df.head())
@@ -27,15 +33,8 @@ print(df.head())
 # Save the DataFrame to a CSV file
 route = '/home/dasxgo/dev/rocket/reports/03-clean.csv'
 df.to_csv(route, index=False)
-
-print(df.head(5))
-print('-' * 160)
-print(f'Total errors in {error_column}: {total_errors}')
-print('-' * 160)
 print(f'DataFrame saved in {route}')
 print('-' * 160)
-print(f'Total errors in {data_okay_column} after cleaning: {total_errors_cleaned}')
-
 
 def main():
     # Read CSV file
