a580db01fb6d912166f809a974d1755479ea7a91
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-23 18:19:50 -0500

    update

--- src/main.py
+++ src/main.py
@@ -3,63 +3,63 @@ import numpy as np
 import utils
 
 # Read CSV file
-df = pd.read_csv('/home/dasxgo/dev/rocket/reports/02-plan.csv')
-print(df.head(5))
+df4 = pd.read_csv('/home/dasxgo/dev/rocket/reports/02-plan.csv')
+print(df4.head(5))
 print('-' * 160)
 
 # Display information about the DataFrame
-print(df.info())
+print(df4.info())
 
 # Check data for errors
 error_column = 'OIL_BBL'
-total_errors = df[(df[error_column] == 0) | (df[error_column] < 0)].shape[0]
+total_errors = df4[(df4[error_column] == 0) | (df4[error_column] < 0)].shape[0]
 print(f'Total errors in {error_column}: {total_errors}')
 print('-' * 160)
 
 # clean the data
 production_columns = ["OIL_BBL", "GAS_MCF", "WATER_BBL"]
-utils.clean_data(df, production_columns)
+utils.clean_data(df4, production_columns)
 
 # Check after cleaning
 data_okay_column = 'OIL_BBL'
-total_errors_cleaned = df[(df[data_okay_column] == 0) | (df[data_okay_column] < 0)].shape[0]
+total_errors_cleaned = df4[(df4[data_okay_column] == 0) | (df4[data_okay_column] < 0)].shape[0]
 print(f'Total errors in {data_okay_column} after cleaning: {total_errors_cleaned}')
 
 print('-' * 160)
 
 # Show first clean records
-print(df.head())
+print(df4.head())
 
 # Save the DataFrame to a CSV file
 route = '/home/dasxgo/dev/rocket/reports/03-clean.csv'
-df.to_csv(route, index=False)
+df4.to_csv(route, index=False)
 print(f'DataFrame saved in {route}')
 print('-' * 160)
 
 def main():
     # Read CSV file
-    df2 = pd.read_csv('/home/dasxgo/dev/rocket/reports/03-clean.csv')
-    print(df2.head(5))
+    df5 = pd.read_csv('/home/dasxgo/dev/rocket/reports/03-clean.csv')
+    print(df5.head(5))
     print('=' * 160)
     
     # Change column target - Options: OIL_BBL / GAS_MCF / WATER_BBL
 
     target = 'OIL_BBL'
 
-    def reservoir(df2, reservoir):
-        reservoir = df2[df2['RESERVOIR'] == reservoir]
+    def reservoir(df5, reservoir):
+        reservoir = df5[df5['RESERVOIR'] == reservoir]
         return reservoir[['WellName', 'TotalProdMonths', target]]
     
     # funtion for the reservoir
-    df_lss = reservoir(df2, "LSS")
+    df_lss = reservoir(df5, "LSS")
     print(df_lss.head())
     print('=' * 160)
 
-    df_wsb = reservoir(df2, "WCB")
+    df_wsb = reservoir(df5, "WCB")
     print(df_wsb.head())
     print('=' * 160)
     
-    df_wca = reservoir(df2, "WCA")
+    df_wca = reservoir(df5, "WCA")
     print(df_wca.head())
     print('=' * 160)
 
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
+import load
 
-def clean_data(df, columns):
-    for col in columns:
-        df.drop(df[(df[col] == 0) | (df[col] < 0)].index, inplace=True)
-
+df = load.df
+df4 = pd.read_csv('/home/dasxgo/dev/rocket/reports/02-plan.csv')
 
 def production_values(df, well_planned):
     production_columns = ['LiquidsProd_BBL', 'GasProd_MCF', 'WaterProd_BBL']
@@ -19,18 +18,20 @@ def production_values(df, well_planned):
     # Convert columns to int64
     for column in production_columns:
         df[column] = df[column].astype(np.int64)
+        return df
 
-    return df
+def clean_data(df4, columns):
+    for col in columns:
+        df4.drop(df4[(df4[col] == 0) | (df4[col] < 0)].index, inplace=True)
 
 if __name__ == '__main__':
-    df = pd.read_csv('/home/dasxgo/dev/rocke/reports/01-df.csv')
     print(df.head(5))
 
     well_planned = 5000
     df = production_values(df, well_planned)
     print(df.head())
-
-# Save the DataFrame to a CSV file
+    
+    # Save the DataFrame to a CSV file
     route = '/home/dasxgo/dev/rocket/reports/02-plan.csv'
     df.to_csv(route, index=False)  
     print(f'DataFrame save in {route}')
